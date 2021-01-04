6eda484c67b1707764efd529bcb4692375057a0d
Brigitte07 <brigitte_roque@usmp.pe>
2021-01-04 14:19:47 -0600

    Practica 1 - creacion de bono universal, aplicacion de modelo, conexion a la base de datos en Heroku.

---
+++ Controllers/BonoControllers.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PRACTICA01.Models;
+using PRACTICA01.Data;
+
+
+namespace PRACTICA01.Controllers
+{
+    public class BonoController : Controller
+    {
+
+       private readonly ILogger<BonoController> _logger;
+        private readonly DatabaseContext _context;
+        public BonoController(ILogger<BonoController> logger,DatabaseContext context)
+        {
+             _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(Bono objContacto){
+            if (ModelState.IsValid)
+            {
+                _context.Add(objContacto);
+                _context.SaveChanges();
+                objContacto.Response = "Fue guardado  con  exito";
+            }
+            return View("index", objContacto);
+        }
+
+    }
+}
---
+++ Models/Bono.cs
@@ -0,0 +1,50 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRACTICA01.Models
+{
+    [Table("personas")]
+    public class Bono
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Column("id")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese Nombre")]
+        [Display(Name="Nombres")]
+        [Column("nombre")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese Apellido")]
+        [Display(Name="Apellidos")]
+        [Column("apellidos")]
+        public String LastName { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese su  Distrito")]
+        [Display(Name="Distrito")]
+        [Column("distrito")]
+        public String Distrito { get; set; }
+
+        [Display(Name="Banco")]
+        [Column("banco")]
+        public String Banco { get; set; }
+        [Display(Name="Edad")]
+        [Column("edad")]
+        public int Edad { get; set; }
+
+        [Display(Name="Sexo")]
+        [Column("sexo")]
+        public String Se { get; set; }
+
+        [Display(Name="Autor")]
+        [Column("autor")]
+        public String Author { get; set; }
+
+        [NotMapped]
+
+        public String Response { get; set; }
+
+    }
+}
